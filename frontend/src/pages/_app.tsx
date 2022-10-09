b30d7f4bd423cc7145d5231229761834beaf6923
Chau Nguyen <nguyenngocchaugv@yahoo.com.vn>
2022-10-10 00:13:24 +0700

    Integrate with apollo-client graphql

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,17 +1,21 @@
 // import '../styles/globals.css'
+import { ApolloProvider } from '@apollo/client';
 import { ChakraProvider } from '@chakra-ui/react';
 import { Session } from 'next-auth';
 import { SessionProvider } from 'next-auth/react';
 import type { AppProps } from 'next/app';
 import { theme } from '../chakra/theme';
+import { client } from '../graphql/apollo-client';
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps<{ session: Session; }>) {
   return (
-    <SessionProvider session={session}>
-      <ChakraProvider theme={theme}>
-        <Component {...pageProps} />
-      </ChakraProvider>
-    </SessionProvider>
+    <ApolloProvider client={client}>
+      <SessionProvider session={session}>
+        <ChakraProvider theme={theme}>
+          <Component {...pageProps} />
+        </ChakraProvider>
+      </SessionProvider>
+    </ApolloProvider>
   );
 }
 
